05c2446d69e2e125c5b5b3c7113c61b610bbf60a
emnabz <1947@holbertonschool.com>
2020-12-10 15:19:39 +0100

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a, b = 1, 2
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    var = add
-    pr = print
-    return a + b
+    print("{} + {} = {}".format(a, b, add(a, b)))

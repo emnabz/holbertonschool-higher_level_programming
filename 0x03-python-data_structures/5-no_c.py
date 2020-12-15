d039e2f84b656316a3df8d319eb7714bd0ef7f5c
emnabz <1947@holbertonschool.com>
2020-12-15 22:57:58 +0100

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new = ""
+    copy_string = ""
     for i in my_string:
         if i != "c" and i != "C":
-            new = new + i
-            return (new)
+            copy_string = copy_string + i
+    return (copy_string)

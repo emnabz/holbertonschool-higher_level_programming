13759a649d078340ddf5a759d3d841b9e85af0c5
emnabz <1947@holbertonschool.com>
2021-01-19 22:08:09 +0100

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class base geometry base on the previous class"""
+
+
+class BaseGeometry():
+    """class base geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")

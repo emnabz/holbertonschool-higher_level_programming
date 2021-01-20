db10ca8b85e7a587020695bc8418f64a7221d6b2
emnabz <1947@holbertonschool.com>
2021-01-20 20:23:13 +0100

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def read_file(filename="", text=""):
-    with open(filename, "r", encoding="utf-8") as file:
+def read_file(filename=""):
+    with open(filename, encoding="utf-8", mode="r") as file:
         print(file.read(), end="")

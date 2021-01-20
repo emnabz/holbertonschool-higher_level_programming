b0ed8e9b28b7e673e9dcc552a7a87daba12cc9f2
emnabz <1947@holbertonschool.com>
2021-01-20 22:58:06 +0100

    task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Student:
+    """description for student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieve info to json format """
+        new = {}
+        if attrs is None:
+            return vars(self)
+        else:
+            for el in attrs:
+                if el in vars(self):
+                    new[el] = vars(self)[el]
+        return new

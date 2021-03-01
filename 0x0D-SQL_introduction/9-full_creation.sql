9b1c779cb9057477ad45931578eef4709aba6437
emnabz <1947@holbertonschool.com>
2021-03-01 20:26:24 +0000

    task9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- creating a new table
+
+CREATE TABLE IF EXISTS second_tabel (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score)
+VALUES
+(1, "Jhon", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);  

21caa5deaca44c0e9bd58db19e0816ab63d8db2a
emna <1947@holbertonschool.com>
2021-03-02 19:37:09 +0100

    task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- a script that creates the database
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL

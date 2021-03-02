d8ff19873dfde2c88bbac53b0977629bb4d40ffb
emna <1947@holbertonschool.com>
2021-03-02 19:48:08 +0100

    task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- a script that creates the database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id) 
+);

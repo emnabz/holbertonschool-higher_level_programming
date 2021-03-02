61c60c25f65768f0b02806b2cb63a2086e02ee41
emna <1947@holbertonschool.com>
2021-03-02 20:54:13 +0100

    task9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
--- a script that lists all cities contained in the database 
+-- list all cities in database
 
-SELECT cities.id, cities.name states.name 
-FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;

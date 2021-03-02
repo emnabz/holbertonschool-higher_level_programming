e5df7a8faffe6db3c5468161568ed556c75a2111
emna <1947@holbertonschool.com>
2021-03-02 17:19:49 +0100

    task2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- a script that creates the database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE  USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@'localhost';

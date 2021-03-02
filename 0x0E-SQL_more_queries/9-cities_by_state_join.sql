3b06ef6fe135e1b969115acac63c20163840aa08
emna <1947@holbertonschool.com>
2021-03-02 20:33:53 +0100

    task9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- a script that lists all cities contained in the database 
+
+SELECT cities.id, cities.name, states.name FROM cities; 
+INNER JOIN states ON cities.state.id = states.id;
+ORDER BY cities.id;

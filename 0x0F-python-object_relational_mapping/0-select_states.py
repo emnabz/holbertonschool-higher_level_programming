b7ea8264e079a6fb5159effb6e1cd9488fb79308
emna <1947@holbertonschool.com>
2021-03-29 11:08:22 +0100

    task0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""create states table in hbtn_0e_0_usa with some data"""
+
+
+import MySQLdb
+import sys
+
+def print_all_states():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", 
+    port=3306, 
+    user=username, 
+    passwd=password, 
+    db=dataBase)
+     
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
+if __name__ == "__main__":
+  print_all_states()

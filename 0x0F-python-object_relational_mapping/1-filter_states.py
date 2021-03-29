a94cd0e0f11acd48aa2101d9836feb03c17f6c42
emna <1947@holbertonschool.com>
2021-03-29 20:33:53 +0100

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N"""
+"""
+Module that lists states with names starts with N
+from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
 
-def print_table_state():
-    """print table state"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dataBase = sys.argv[3]
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=dataBase)
-    cur = db.cursor()
-    cur.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-    names = cur.fetchall()
-    for name in names:
-        print(name)
+def print_all_names():
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3],
+                               charset="utf8"
+                               )
+    cur = database.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for i in row:
+        if i[1][0] == 'N':
+            print(i)
     cur.close()
-    db.close()
+    database.close()
 
 
 if __name__ == "__main__":
-    print_table_state()
+    print_all_names()

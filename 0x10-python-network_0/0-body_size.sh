54d38b48bf4cef4d046dc2a840742557f52db578
emna <1947@holbertonschool.com>
2021-04-10 16:09:10 +0100

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # A bash script that in a URL sends a request to that URL, and displays the size of the body of the response
-echo $"$(curl -sI "$1" | grep 'Content-Length' | cut  -d ':' -f '2' | cut -d ' ' -f '2')"
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2

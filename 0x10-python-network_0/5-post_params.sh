1caa1de30a713805be545b56830402290b6c151b
emna <1947@holbertonschool.com>
2021-04-11 21:01:25 +0100

    task5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a POST request to the passed
-# URL, and displays the body of the response
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# a Bash script that takes in a URL, sends a POST request to the passed URL
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"

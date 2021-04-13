501eaa5d62f4b77b3ffaf66e052d26a1fccef262
emna <1947@holbertonschool.com>
2021-04-13 23:51:00 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
+if __name__ == "__main__":
 import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
